3c84b5e9acbd92d7ac724e016ac278c00bf49019
sipra <sipramishra085@gmail.com>
2023-03-17 21:32:18 +0530

    Added program file

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Day8LineComparisionProblem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Line Computation Problems");
+            //LineDiffrence Difference = new LineDiffrence();
+            //Difference.LineDistance();
+
+            //CheckLineEquality Equality = new CheckLineEquality();
+            //Equality.LineEquality();
+
+            CompareTwoLine twoLines = new CompareTwoLine();
+            twoLines.CompareLines();
+        }
+    }
+}
